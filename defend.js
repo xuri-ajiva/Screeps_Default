6585939e08a23974d2ee17e76865672fc71937ce
xuri <T0Bl4S.durst@gmail.com>
2019-12-05 21:23:53 +0100

    tower heal

--- defend.js
+++ defend.js
@@ -1,3 +1,5 @@
+const max_hits = 100000;
+
 let tower = {
     /** @param {Room} room
      **/
@@ -7,7 +9,21 @@ let tower = {
             let towers = room.find(FIND_MY_STRUCTURES, {filter: {structureType: STRUCTURE_TOWER}});
             towers.forEach(tower => tower.attack(enemy[0]));
 
-            console.log('defending: '+ towers.length + enemy[0].pos);
+            console.log('defending: ' + towers.length + enemy[0].pos);
+        } else {
+            let ramp = room.find(FIND_STRUCTURES, {filter: (s) => s.structureType === STRUCTURE_RAMPART && s.hits < max_hits});
+            ramp.sort((a, b) => a.hits - b.hits);
+            if (ramp.length > 0) {
+                let towers = room.find(FIND_MY_STRUCTURES, {filter: {structureType: STRUCTURE_TOWER}});
+                towers.forEach(tower => tower.repair(ramp[0]));
+            } else {
+                let creeps = room.find(FIND_CREEPS, {filter: (c) => c.hits < c.hitsMax});
+                creeps.sort((a, b) => a.hits - b.hits);
+                if (creeps.length > 0) {
+                    let towers = room.find(FIND_MY_STRUCTURES, {filter: {structureType: STRUCTURE_TOWER}});
+                    towers.forEach(tower => tower.heal(creeps[0]));
+                }
+            }
         }
     }
 
